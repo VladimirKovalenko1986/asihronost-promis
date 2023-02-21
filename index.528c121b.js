9c7d6abdabd43ff60f3c40f1f81e37efd4c418c0
VladimirKovalenko1986 <110353203+VladimirKovalenko1986@users.noreply.github.com>
2023-02-21 14:30:14 +0000

    Deploying to gh-pages from @ VladimirKovalenko1986/asihronost-promis@1453e71b6c6be79c387d09e02667c4fc64afe1c1 🚀

--- index.528c121b.js
+++
@@ -1,2 +0,0 @@
-({start(){const t=Date.now();setInterval((()=>{const e=function(t){const e=Math.floor(t%864e5/36e5),o=Math.floor(t%36e5/6e4),n=Math.floor(t%6e4/1e3);return{hours:e,mins:o,secs:n}}(Date.now()-t);console.log("🚀 ~ setInterval ~ timeComponents:",e)}),1e3)}}).start();
-//# sourceMappingURL=index.528c121b.js.map
