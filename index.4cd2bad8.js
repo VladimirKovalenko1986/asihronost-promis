3abf7f252de971f11a39b814a3c228d780018112
VladimirKovalenko1986 <110353203+VladimirKovalenko1986@users.noreply.github.com>
2023-02-22 20:14:23 +0000

    Deploying to gh-pages from @ VladimirKovalenko1986/asihronost-promis@5412312000c7667a4efbfe34ea3b5a9d5941f05d 🚀

---
+++ index.4cd2bad8.js
@@ -0,0 +1,2 @@
+document.getElementById("time"),document.getElementById("stopBtn");
+//# sourceMappingURL=index.4cd2bad8.js.map
---
+++ index.7b907c62.js
@@ -0,0 +1,2 @@
+document.getElementById("time"),document.getElementById("stopBtn");
+//# sourceMappingURL=index.7b907c62.js.map
